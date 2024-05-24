bd3d3596b0ad7b2b8e1f13685e232a64225c9bc4
Vicente Bermúdez <thevincentes@gmail.com>
2024-05-24 10:33:53 -0400

    CACE-0 | Remove CACE-related modules (#19)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,13 @@
 import { Module, Logger } from '@nestjs/common';
-import { MembersModule } from './members/members.module';
 import { KnexModule } from './db/knex.module';
-import { MembersController } from './members/members.controller';
-import { MembersService } from './members/members.service';
-import { EventsModule } from './events/events.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { HttpModule } from '@nestjs/axios';
-import { EventbriteModule } from './eventbrite/eventbrite.module';
 import { IssuerAgentController } from './ssi/issuerAgent.controller';
 import { IssuerAgentModule } from './ssi/issuerAgent.module';
 import { DWNModule } from './ssi/dwn/dwn.module';
 import { AUTHORIZED_CALLER_TOKEN } from './ssi/dwn/authorized-caller.provider';
 import { DWNController } from './ssi/dwn/dwn.controller';
-import { InvitationService } from './invitation/invitation.service';
-import { InvitationModule } from './invitation/invitation.module';
 
 const ENV = process.env.NODE_ENV;
 const envFilePath = [!ENV ? '.env' : `.env.${ENV}`];
@@ -26,19 +19,13 @@ const envFilePath = [!ENV ? '.env' : `.env.${ENV}`];
       isGlobal: true,
       load: [configuration],
     }),
-    MembersModule,
     KnexModule,
     IssuerAgentModule,
-    EventsModule,
     HttpModule,
-    EventbriteModule,
     DWNModule,
-    InvitationModule,
   ],
-  controllers: [MembersController, IssuerAgentController, DWNController],
+  controllers: [IssuerAgentController, DWNController],
   providers: [
-    MembersService,
-    InvitationService,
     Logger,
     {
       provide: AUTHORIZED_CALLER_TOKEN,
