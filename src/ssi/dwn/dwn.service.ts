9c5defd5a03945ff9a8f9a522e0c9475c75022c5
Romina <rsalveraglio@infuy.com>
2024-08-28 13:01:22 -0300

    removed unused import, updated web5 package to see if that solved a disclaimer (it did not)

--- src/ssi/dwn/dwn.service.ts
+++ src/ssi/dwn/dwn.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Record, RecordsQueryResponse, Web5 } from '@web5/api';
 import * as fs from 'fs';
-import { BearerDid } from '@web5/dids';
 import { VerifiableCredential } from '@web5/credentials';
 
 export interface CredentialQueryResultObject {
